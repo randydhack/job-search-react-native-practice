3157c555ee18945810a0bf48f813237e4faa4266
randy hac <randydhacks@gmail.com>
2024-01-06 19:47:38 -0800

    pill filter section ui done

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -12,8 +12,13 @@ import { useRouter } from "expo-router";
 import styles from "./welcome.style";
 import { icons, SIZES } from "../../../constants";
 
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
+
 const Welcome = () => {
   const router = useRouter();
+
+  const [activeJobType, setActiveJobType] = useState("Full-time");
+
   return (
     <View>
       <View style={styles.container}>
@@ -23,8 +28,41 @@ const Welcome = () => {
 
       <View style={styles.searchContainer}>
         <View style={styles.searchWrapper}>
-          <TextInput style={styles.searchInput} value={''} onChange={() => {}} placeholder="What are you looking for?"/>
+          <TextInput
+            style={styles.searchInput}
+            value={""}
+            onChange={() => {}}
+            placeholder="What are you looking for?"
+          />
         </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        {/* Flatlist is a mapping component*/}
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`);
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={item => item} // what you would do in react giving unique key
+          contentContainerStyle={{columnGap: SIZES.small}}
+          horizontal
+        />
       </View>
     </View>
   );
